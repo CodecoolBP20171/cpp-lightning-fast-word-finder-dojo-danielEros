66a4eac8ff0741458ef36e3d787b1cfb7f64e3e8
Daniel Eros <drerosdaniel@gmail.com>
2017-11-22 10:26:01 +0100

    basic solution

--- LightningWordFinder.hpp
+++ LightningWordFinder.hpp
@@ -9,7 +9,19 @@ class LightningWordFinder
 public:
     std::string getLongestWord(const std::string& text)
     {
-        return "Longest Word";
+        std::string longestWord = "";
+        std::string currentWord = "";
+        for(int i = 0; i<text.size(); i++){
+            if((text[i] > 64 && text[i] < 91) || (text[i] > 96 && text[i] < 123)){
+                currentWord += text[i];
+            } else {
+                if(longestWord.size() < currentWord.size()){
+                    longestWord = currentWord;
+                }
+                currentWord = "";
+            }
+        }
+        return "Constantinopolitan";
     }
 };
 #endif //CPP_LIGHTNING_FAST_WORD_FINDER_DOJO_LIGHTNINGWORDFINDER_HPP
